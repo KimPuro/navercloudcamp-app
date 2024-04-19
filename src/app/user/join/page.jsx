e61abe4f34547ec31d3a7caaf56d1aa6c60e5f05
KimPuro <eos002@naver.com>
2024-04-19 18:59:21 +0900

    fix : join page for build

---
+++ src/app/user/join/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+import WIthCheckboxes from "@/components/tables/WithCheckboxes";
+import WithRightAlignedNav from "@/components/headers/WithRightAlignedNav";
+
+export default function Home() {
+
+    return (
+        <>
+            <WithRightAlignedNav/>
+            <WIthCheckboxes/>
+        </>
+    );
+}
