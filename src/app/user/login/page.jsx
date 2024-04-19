3f97ef5f7747f10aeb959e9c9d2ae6490bbceb03
KimPuro <eos002@naver.com>
2024-04-19 18:23:16 +0900

    feat : add axios post

--- src/app/user/login/page.jsx
+++ src/app/user/login/page.jsx
@@ -1,13 +1,23 @@
+'use client'
 import SimpleCard from "@/components/signInForms/SimpleCard";
 import CenteredWithSingleAction from "@/components/dialogs/CenteredWithSingleAction";
 import WithRightAlignedNav from "@/components/headers/WithRightAlignedNav";
+import axios from "axios";
+import {axiosPostLogin} from "@/libs/axiosAPI";
 
 export default function Home() {
+
+  const a = "qwerty"
+  const b = "1234"
+  const go = ()=>{
+  axiosPostLogin(a,b,"/users/login")
+    }
   return (
 <>
   <WithRightAlignedNav/>
     <SimpleCard/>
   <CenteredWithSingleAction/>
+  <div onClick={go}>dsfadfadfadfadfadf</div>
 </>
   );
 }
