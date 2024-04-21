8fe1eb0a8c88a52c87dbe6872bb323fd165c85eb
KimPuro <puro@protonmail.ch>
2024-04-22 02:17:55 +0900

    feat : add test page, api route ,not-found page, parallel route

--- src/app/test2/page.jsx
+++ src/app/test2/page.jsx
@@ -1,18 +1,10 @@
-'use client'
-import React from "react";
-import axios from "axios";
-import Example from "@/components/tables/WithCheckboxes";
-import {axiosGet} from "@/libs/axiosAPI";
 
-export default function Test2() {
-    const go = async () => { // go 함수를 async 함수로 선언
-        let arr = await axiosGet("users/list"); // getUser의 결과를 await로 기다림
-        console.log(arr); // 비동기 처리가 완료된 데이터를 콘솔에 출력
-    }
 
+import SimpleCard from "@/app/test/page";
+
+const Homeee = () =>{
     return (
-        <>
-            <div onClick={go}>aaaaaaaaaaaaaaaaaaaaa</div>
-        </>
-    );
+        SimpleCard()
+    )
 }
+export default Homeee;
--- src/app/user/login/page.jsx
+++ src/app/user/login/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
 <>
   <WithRightAlignedNav/>
-    <SimpleCard/>
-  <CenteredWithSingleAction/>
+{/*    <SimpleCard/>
+  <CenteredWithSingleAction/>*/}
 </>
   );
 }
