953d7ab918cf14af2919166798b63d73d8920e3f
KimPuro <eos002@naver.com>
2024-04-24 18:04:04 +0900

    feat : add board list

---
+++ src/app/(afterLogin)/board/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+import WIthCheckboxes from "@/components/tables/WithCheckboxes";
+import WithRightAlignedNav from "@/components/headers/WithRightAlignedNav";
+import WithVerticalLines from "@/components/lists/WithVerticalLines";
+
+export default function Test2() {
+
+    return (
+        <>
+            <WithRightAlignedNav/>
+            <WithVerticalLines/>
+        </>
+    );
+}
