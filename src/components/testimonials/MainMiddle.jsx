e2b08286c059c5c7981107d691a3c7c25730f157
KimPuro <puro@protonmail.ch>
2024-06-02 19:50:46 +0900

    fix : edit team name

--- src/components/testimonials/MainMiddle.jsx
+++ src/components/testimonials/MainMiddle.jsx
@@ -4,7 +4,7 @@ export default function MainMiddle() {
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto grid max-w-2xl grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                     <div className="flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 xl:pr-20">
-                        <img className="h-12 self-start" src="https://tailwindui.com/img/logos/tuple-logo-gray-900.svg" alt="" />
+                        <h1 className="font-bold text-3xl">Tetrips</h1>
                         <figure className="mt-10 flex flex-auto flex-col justify-between">
                             <blockquote className="text-lg leading-8 text-gray-900">
                                 <p>
@@ -25,7 +25,7 @@ export default function MainMiddle() {
                         </figure>
                     </div>
                     <div className="flex flex-col border-t border-gray-900/10 pt-10 sm:pt-16 lg:border-l lg:border-t-0 lg:pl-8 lg:pt-0 xl:pl-20">
-                        <img className="h-12 self-start" src="https://tailwindui.com/img/logos/reform-logo-gray-900.svg" alt="" />
+                        <h1 className="font-bold text-3xl">Tetrips</h1>
                         <figure className="mt-10 flex flex-auto flex-col justify-between">
                             <blockquote className="text-lg leading-8 text-gray-900">
                                 <p>
